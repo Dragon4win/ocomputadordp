8db7e8b85295b708d38b4e26e51b262e83466ff1
Daniel Pereira <danielfcpereira@outlook.com>
2025-11-12 15:58:44 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="meuestilo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplos Fixes </title>
+</head>
+<body><center>
+    <h1>Feliz aniversario!!!</h1><div id="erro"></div>
+    <p>Este plugin vai permitir congratular o aniversariante numa pag web ou numa app.</p>
+    <form method="post">
+        <input type="text" name="nome" id="nome" placeholder="Primeirp e Ultimo">
+        <input type="submit" name="subpara" id="subpara" value="Enviar">
+    </form>
+    <?php if(isset($_POST['subpara'])){
+        $nome = $_POST["nome"];
+        session_start();
+        $_SESSION["nome"] = $nome;
+        echo "<script>document.location.href = 'http://localhost/git/exemplos.php?parabens=parabens'</script>";
+    }
+    ?>
+    
+    <hr>
+    <h1>Jogo do galo</h1>
+    <div id="tab"></div>
+    <hr>
+    <script src="http://localhost/git/meujs.js"></script></center>
+</body>
+</html>
